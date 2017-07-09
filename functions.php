46b5f91f3d48bff13d5b96c0025cb50283e20ab5
elsr <elsr@list.ru>
2017-07-09 15:20:38 +0500

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,28 @@
+<?php
+/** Add theme support */
+require get_template_directory() . '/includes/theme-support.php';
+/** Enqueue scripts */
+require get_template_directory() . '/includes/enqueue-scripts-style.php';
+/** Various clean up functions */
+require get_template_directory() . '/includes/cleanup.php';
+/** Return entry meta information for posts */
+require get_template_directory() . '/includes/meta-data.php';
+/** Create widget areas in sidebar and footer */
+require get_template_directory() . '/includes/widget-areas.php';
+/** Add register nav menu */
+require get_template_directory() . '/includes/navigation.php';
+/** Add Carbon Fields */
+require get_template_directory() . '/includes/carbon-fields/carbon-fields-plugin.php';
+add_action( 'carbon_register_fields', 'ast_register_custom_fields' );
+function ast_register_custom_fields() {
+	require get_template_directory() . '/includes/custom-fields/post-meta.php';
+	require get_template_directory() . '/includes/custom-fields/theme-options.php';
+}
+/** Add Woocommerce files */
+if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+	require get_template_directory() . '/woocommerce/includes/wc-cart-functions.php';
+	require get_template_directory() . '/woocommerce/includes/wc-checkout-functions.php';
+	require get_template_directory() . '/woocommerce/includes/wc-custom-fields.php';
+	require get_template_directory() . '/woocommerce/includes/wc-function.php';
+	require get_template_directory() . '/woocommerce/includes/wc-remove-functions.php';
+}
